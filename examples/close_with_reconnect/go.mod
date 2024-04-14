5c813277cbc6c7975414e8336768ffe533b52354
revell29 <diraapsya0@gmail.com>
2024-04-14 23:27:57 +0700

    feat(client): add new func Consume

--- examples/close_with_reconnect/go.mod
+++ examples/close_with_reconnect/go.mod
@@ -1,8 +1,13 @@
-module github.com/apsyadira-jubelio/go-amqp-reconnect/example
+module rabbitmq
 
 go 1.20
 
 require (
-	github.com/apsyadira-jubelio/go-amqp-reconnect/rabbitmq v0.0.0-20240414085239-5708d84eb0f2
+	github.com/apsyadira-jubelio/go-amqp-reconnect/rabbitmq v0.0.0-20240414094204-454afb52b150
+	github.com/gammazero/workerpool v1.1.3
 	github.com/streadway/amqp v1.1.0
 )
+
+require github.com/gammazero/deque v0.2.0 // indirect
+
+replace github.com/apsyadira-jubelio/go-amqp-reconnect/rabbitmq v0.0.0-20240414094204-454afb52b150 => ../../rabbitmq
--- examples/close_with_reconnect/main.go
+++ examples/close_with_reconnect/main.go
@@ -1,75 +1,128 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/apsyadira-jubelio/go-amqp-reconnect/rabbitmq"
+	"github.com/gammazero/workerpool"
+	"github.com/streadway/amqp"
 )
 
 func main() {
 	rabbitmq.Debug = true
 
-	rmqCliet := rabbitmq.NewRmqClient()
+	rmqCliet := rabbitmq.NewRmqClient(&rabbitmq.ChannelOptions{
+		ConnectionString: "amqp://guest:guest@localhost:5672/",
+		QueueName:        "test-auto-delete",
+	})
+
+	queueName := "test-auto-delete"
+	queueTTL := 1500000
+	queueDurable := true
+	queueExchange := fmt.Sprintf("%s-exchange", queueName)
+	deadQueue := fmt.Sprintf("%s-dead-queue", queueName)
+	DeadExchange := fmt.Sprintf("%s-dead-exchange", queueName)
+	deadQueueDurable := false
+	deadQueueTTL := 1500000
+
+	err := rmqCliet.OpenChannel(&rabbitmq.ChannelOptions{
+		ConnectionString: "amqp://guest:guest@localhost:5672/",
+		QueueName:        "test-auto-delete",
+		QueueTTL:         queueTTL,
+		QueueDurable:     queueDurable,
+		QueueExchange:    queueExchange,
+		DeadQueue:        deadQueue,
+		DeadExchange:     DeadExchange,
+		DeadQueueDurable: deadQueueDurable,
+		DeadQueueTTL:     deadQueueTTL,
+		QueueRoutingKey:  "test-auto-delete",
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	go func() {
+		for {
+			err := rmqCliet.SendMessage(map[string]interface{}{
+				"message": "hello world",
+			},
+				queueName,
+				amqp.Publishing{
+					ContentType: "text/plain",
+				})
+
+			if err != nil {
+				log.Println(err)
+			}
+
+			log.Println("publish message")
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	// Create a context that is cancelled on system interrupt or SIGTERM signal
+	ctx, closeCtx := context.WithCancel(context.Background())
+	defer closeCtx()
+
+	consumerDone := make(chan bool, 1)
+
+	// Create a worker pool with a specified number of workers
+	wp := workerpool.New(1)
+	defer wp.Stop() // Ensure all workers are stopped before exiting
+
+	go func() {
+		d, err := rmqCliet.Consume()
+		if err != nil {
+			log.Panic(err)
+		}
+
+		for {
+			stop := false
+
+			select {
+			case <-ctx.Done():
+				stop = true
+			case msg := <-d:
+				wp.Submit(func() {
+					log.Printf("msg: %s", string(msg.Body))
+					// Acknowledge the message
+					msg.Ack(false)
+				})
+			}
+
+			if stop {
+				break
+			}
+		}
+
+		consumerDone <- true
+	}()
+
+	signals := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		s := <-signals
+		log.Printf("received signal: %v", s)
+		closeCtx()
+
+		<-consumerDone
+		rmqCliet.Close()
+
+		done <- true
+	}()
+
+	log.Printf("awaiting signal")
+	<-done
+	wp.StopWait()
+	log.Printf("exiting")
 }
-
-// func main() {
-// 	rabbitmq.Debug = true
-
-// 	url := "amqp://guest:guest@localhost:5672/"
-// 	flag.Parse()
-
-// 	conn, err := rabbitmq.Dial(url)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	sendCh, err := conn.Channel()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	consumeCh, err := conn.Channel()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	err = consumeCh.Qos(1, 0, false)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	_, err = consumeCh.QueueDeclare("test-auto-delete", false, true, false, true, nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	go func() {
-// 		for {
-// 			err := sendCh.Publish("", "test-auto-delete", false, false, amqp.Publishing{
-// 				ContentType: "text/plain",
-// 				Body:        []byte(time.Now().String()),
-// 			})
-// 			if err != nil {
-// 				log.Panic(err)
-// 			}
-
-// 			log.Println("publish message")
-// 			time.Sleep(time.Second * 5)
-// 		}
-// 	}()
-
-// 	go func() {
-// 		d, err := consumeCh.Consume("test-auto-delete", "", false, false, false, false, nil)
-// 		if err != nil {
-// 			log.Panic(err)
-// 		}
-
-// 		for msg := range d {
-// 			log.Printf("msg: %s", string(msg.Body))
-// 			time.Sleep(time.Second * 2)
-// 			log.Printf("ack, err: %v", msg.Ack(false))
-// 		}
-// 	}()
-
-// 	wg := sync.WaitGroup{}
-// 	wg.Add(1)
-// 	wg.Wait()
-// }
