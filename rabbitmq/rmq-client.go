e2dbc43bd1e8931dff3d9f9bab08b261774e53dd
apsya Dira <diraapsya0@gmail.com>
2025-01-12 03:18:32 +0700

    feat: add exchange options to declare exchange

--- rabbitmq/rmq-client.go
+++ rabbitmq/rmq-client.go
@@ -149,7 +149,7 @@ func (client *RmqClient) OpenChannel(options *ChannelOptions) error {
 			false,
 			false,
 			false,
-			nil,
+			options.QueueExchangeOptions,
 		)
 		if err != nil {
 			return err
